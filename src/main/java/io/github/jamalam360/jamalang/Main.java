c9ff9518c596d40488e1873f8e4f03ced4610887
Jamalam360 <56727311+Jamalam360@users.noreply.github.com>
2021-09-08 21:56:17 +0100

    todo!

--- src/main/java/io/github/jamalam360/jamalang/Main.java
+++ src/main/java/io/github/jamalam360/jamalang/Main.java
@@ -9,4 +9,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         new JamalangInterpreter().execute(Files.readString(new File(args[0]).toPath()));
     }
+
+    //region To-Do:
+
+    //TODO: Clean up and split up ParsingHelper into its own categories
+    //TODO: Abstract variable management into its own class
+    //TODO: String support
+    //TODO: Mathematical Expression Evaluator
+    //TODO: Nested Loops
+    //TODO: While loops
+    //TODO: Validation of parameters
+    //TODO: Classes
+    //TODO: Custom functions
+
+    //endregion
 }
