45b2bed1e488138d16645e961f6d8dc5693f2ffe
James (Jamalam) <james@jamalam.tech>
2023-03-30 15:34:11 +0100

    feat: initial v2 commit :)

---
+++ language/src/main/java/io/github/jamalam360/modifier/language/Main.java
@@ -0,0 +1,11 @@
+package io.github.jamalam360.modifier.language;
+
+import io.github.jamalam360.modifier.language.GrammarParser;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String input = "hello";
+        GrammarParser parser = new GrammarParser();
+    }
+}
---
+++ language/src/main/java/io/github/jamalam360/modifier/language/ModifierLanguage.java
@@ -0,0 +1,2 @@
+package io.github.jamalam360.modifier.language;public class ModifierLanguage {
+}
